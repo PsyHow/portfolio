cf48efdcc7d89c72e87589fb2de3a1c1edb4f65a
PsyHow <viktorburnyshev@gmail.com>
2021-10-25 23:01:05 +0600

    aad

---
+++ src/MyProjects/MyProjects.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './MyProjects.module.css'
+
+export const MyProjects = () => {
+    return (
+        <div className={style.myProjects}>
+
+        </div>
+    );
+}
