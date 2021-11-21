0b6b79528d35df364cc5f5ac3446f4aa1e0d99cc
PsyHow <viktorburnyshev@gmail.com>
2021-11-21 23:44:09 +0600

    add mixin and variables

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -1,19 +1,19 @@
-import style from "./Footer.module.css"
+import style          from "./Footer.module.css"
 import styleContainer from "../common/styles/Container.module.css";
-import React from "react";
+import React          from "react";
 
 export const Footer = () => {
     return (
-        <div className={style.footerBlock}>
-            <div className={`${styleContainer.container} ${style.container}`}>
-                <h2 className={style.title}>Burnishev Viktor</h2>
-                <div className={style.socialContainer}>
-                    <div className={style.social}></div>
-                    <div className={style.social}></div>
-                    <div className={style.social}></div>
-                    <div className={style.social}></div>
+        <div className={ style.footerBlock }>
+            <div className={ `${ styleContainer.container } ${ style.container }` }>
+                <h2 className={ style.title }>Burnishev Viktor</h2>
+                <div className={ style.socialContainer }>
+                    <div className={ style.social }></div>
+                    <div className={ style.social }></div>
+                    <div className={ style.social }></div>
+                    <div className={ style.social }></div>
                 </div>
-                <span>2021</span>
+                <span>Copyright © 2021. All rights reserved.</span>
             </div>
         </div>
     );
