35110652be46eb9d1a5e51d8359a606886ab97f1
PsyHow <viktorburnyshev@gmail.com>
2021-11-22 20:24:59 +0600

    refactor footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,20 +1,21 @@
 import style          from "./Footer.module.scss"
 import styleContainer from "../common/styles/Container.module.css";
 import React          from "react";
-import {Title} from "../common/components/Title/Title";
+import { Title }      from "../common/components/Title/Title";
 
 export const Footer = () => {
     return (
         <div className={ style.footerBlock }>
             <div className={ `${ styleContainer.container } ${ style.container }` }>
-                <Title text={'Burnishev Viktor'}/>
+                {/*<Title text={ 'Burnishev Viktor' }/>*/}
+                <span className={style.title}>Burnishev Viktor</span>
                 <div className={ style.socialContainer }>
                     <div className={ style.social }></div>
                     <div className={ style.social }></div>
                     <div className={ style.social }></div>
                     <div className={ style.social }></div>
                 </div>
-                <p className={style.about}>Copyright © 2021. All rights reserved.</p>
+                <p className={ style.about }>Copyright © 2021. All rights reserved.</p>
             </div>
         </div>
     );
