f16e225c1cd99c0559dba5ae141e0411e2de95e4
PsyHow <viktorburnyshev@gmail.com>
2021-12-03 21:42:56 +0600

    rollback to old version, complete footer

--- src/skills/MySkills.tsx
+++ src/skills/MySkills.tsx
@@ -1,33 +1,35 @@
-import React          from 'react';
-import style          from './MySkills.module.scss'
+import React from 'react';
+import style from './MySkills.module.scss'
 import containerStyle from "../common/styles/Container.module.css";
-import { Skill }      from "./skill/Skill";
-import jsImage        from "../assets/image/js.png";
-import htmlImage      from "../assets/image/html.png";
-import reactImage     from "../assets/image/React.jpg";
+import {Skill} from "./skill/Skill";
+import {Title} from "../common/components/Title/Title";
+import jsImage from "../assets/image/js.png";
+import htmlImage from "../assets/image/html.png";
+import reactImage from "../assets/image/react.png";
 
 export const MySkills = () => {
 
     const javaScript = {
-        backgroundImage: `url(${ jsImage })`,
+        backgroundImage: `url(${jsImage})`
     }
     const html = {
-        backgroundImage: `url(${ htmlImage })`,
+        backgroundImage: `url(${htmlImage})`
     }
     const react = {
-        backgroundImage: `url(${ reactImage })`,
+        backgroundImage: `url(${reactImage})`
     }
 
     return (
-        <div id={ 'skills' } className={ style.skillsBlock }>
-            <div className={ `${ containerStyle.container } ${ style.skillsContainer }` }>
-                <div className={ style.skills }>
-                    <Skill style={ javaScript } title={ 'JavaScript' }
-                           description={ 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }/>
-                    <Skill style={ html } title={ 'Html & Css' }
-                           description={ 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }/>
-                    <Skill style={ react } title={ 'React' }
-                           description={ 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }/>
+        <div id={'skills'} className={style.skillsBlock}>
+            <div className={`${containerStyle.container} ${style.skillsContainer}`}>
+                <Title text={'Skills'}/>
+                <div className={style.skills}>
+                    <Skill style={javaScript} title={'JavaScript'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}/>
+                    <Skill style={html} title={'Html & Css'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}/>
+                    <Skill style={react} title={'React'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}/>
                 </div>
             </div>
         </div>
