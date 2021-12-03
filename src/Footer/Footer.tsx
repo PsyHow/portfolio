c04a7d609e955b3d95877298cad30f3e7e4528e1
PsyHow <viktorburnyshev@gmail.com>
2021-12-03 23:57:28 +0600

    add icon for footer and change main Photo

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,19 +1,35 @@
-import style          from "./Footer.module.scss"
+import style from "./Footer.module.scss"
 import styleContainer from "../common/styles/Container.module.css";
-import React          from "react";
-import { Title }      from "../common/components/Title/Title";
+import React from "react";
+import gmailImg from "../assets/image/gmail.png";
+import telegramImg from "../assets/image/telegram.png";
+import linkedinImg from "../assets/image/linkedin.png";
+import githubImg from "../assets/image/github.png";
 
 export const Footer = () => {
+
+    const gmail = {
+        backgroundImage: `url(${ gmailImg })`,
+    }
+    const telegram = {
+        backgroundImage: `url(${ telegramImg })`,
+    }
+    const linkedIn = {
+        backgroundImage: `url(${ linkedinImg })`,
+    }
+    const github = {
+        backgroundImage: `url(${ githubImg })`,
+    }
+
     return (
         <div className={ style.footerBlock }>
             <div className={ `${ styleContainer.container } ${ style.container }` }>
-                {/*<Title text={ 'Burnishev Viktor' }/>*/}
-                <span className={style.title}>Burnishev Viktor</span>
+                <span className={ style.title }>Burnishev Viktor</span>
                 <div className={ style.socialContainer }>
-                    <div className={ style.social }></div>
-                    <div className={ style.social }></div>
-                    <div className={ style.social }></div>
-                    <div className={ style.social }></div>
+                    <div className={ style.social } style={gmail}></div>
+                    <div className={ style.social } style={telegram}></div>
+                    <div className={ style.social } style={linkedIn}></div>
+                    <div className={ style.social } style={github}></div>
                 </div>
                 <p className={ style.about }>Copyright © 2021. All rights reserved.</p>
             </div>
