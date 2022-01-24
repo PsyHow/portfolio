717659d6ab8c918f8229c361511d11746b9bd78e
PsyHow <viktorburnyshev@gmail.com>
2022-01-24 22:19:57 +0600

    merge

--- src/common/components/Title/Title.tsx
+++ src/common/components/Title/Title.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import style from './Title.module.scss'
 
 type PropsType = {
-    subtitle: string
-    title: string
-    description: string
+    subtitle?: string
+    text: string
+    description?: string
 }
 export const Title = (props: PropsType) => {
     return (
         <div className={style.main}>
             <h3 className={ style.subtitle }>{props.subtitle}</h3>
-            <h3 className={ style.title }>{props.title}</h3>
+            <h3 className={ style.title }>{props.text}</h3>
             <p className={ style.description }>{props.description}</p>
         </div>
     );
